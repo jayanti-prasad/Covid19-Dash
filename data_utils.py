9e325e540919f8e0f8228a7cb127451a6014e1ce
Jayanti Prasad <prasad.jayanti@gmail.com>
2020-12-02 10:33:59 +0530

    daily updates

--- data_utils.py
+++ data_utils.py
@@ -11,7 +11,7 @@ else:
 
 Months={'Jan':'01','Feb':'02','Mar':'03','Apr':'04',\
    'May':'05','Jun':'06','Jul':'07','Aug':'08',\
-   'Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+   'Sept':'09','Oct':'10','Nov':'11','Dec':'12'}
 
 
 def date_formatting(date_string):
